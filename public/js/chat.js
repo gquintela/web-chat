05f043ee7887249a462ef453dbf90a244c558905
gquintela <gquintela@dc.uba.ar>
2020-04-11 16:45:57 -0300

    disable and enable dom elements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,31 +1,51 @@
-const socket = io()
+const socket = io();
 
+//elements
+const $messageForm = document.querySelector("#message-form");
+var $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $locationButton = document.querySelector("#sendLocation");
 
 ////send message
-document.querySelector("#messageForm").addEventListener("submit", (e) => {
-    e.preventDefault()
-    const message = e.target.elements.message.value
-    socket.emit("sendMessage", message, (callbackMsg) => {
-        console.log(callbackMsg)
-    })
-})
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //disable send msg
+  $messageFormButton.setAttribute("disabled", "disabled");
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message, (callbackMsg) => {
+    //enable send msg
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    console.log(callbackMsg);
+  });
+});
 
 ///send location
 document.querySelector("#sendLocation").addEventListener("click", () => {
-    if (!navigator.geolocation) {
-        return alert("Geolocalization is not supported by your browser")
-    }
+  // disable btn
+  $locationButton.setAttribute("disabled", "disabled");
+  if (!navigator.geolocation) {
+    return alert("Geolocalization is not supported by your browser");
+  }
 
-    navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit("sendLocation", ({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        }), (callbackMsg) => {
-            console.log(callbackMsg)
-        })
-    })
-})
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      (callbackMsg) => {
+        //enable button
+        $locationButton.removeAttribute("disabled");
+
+        console.log(callbackMsg);
+      }
+    );
+  });
+});
 
 socket.on("message", (msg) => {
-    console.log(msg)
-})
+  console.log(msg);
+});
